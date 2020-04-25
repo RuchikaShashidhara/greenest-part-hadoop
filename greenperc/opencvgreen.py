0bbd69382ecf9eb8d028304481f664ad7afaebf8
Ruchika Shashidhara <48530409+RuchikaShashidhara@users.noreply.github.com>
2020-04-25 21:14:11 +0530

    data.txt is created with relative path

--- greenperc/opencvgreen.py
+++ greenperc/opencvgreen.py
@@ -5,7 +5,7 @@ import os
 def green_prec(image_path):
 	img = cv2.imread(image_path)
 	#images are converted to 16:9 dimensions
-	img = cv2.resize(img, ((800,450)))
+	img = cv2.resize(img,(800,450))
 	count = 0
 	for x in range(800):
 		for y in range(450):
@@ -20,17 +20,16 @@ locations_paths = [x[0] for x in os.walk(images_path)]
 
 for i in range(1, len(locations_paths)):
 	location_path = locations_paths[i]
-	#location = location_path[len(images_path)+1:]
+	location = location_path[len(images_path)+1:]
+	img_location = "/images/" + location
 	for root, direcs, files in os.walk(location_path):
 		for filename in files:	
-			print(filename)
-			print(location_path)
-			#print(location)
-			print(green_prec(location_path + "/" + filename))	
-			file_data.write(filename + "," + location_path + "," + 	green_prec(location_path + "/" + filename) + "\n")	
+			print(filename)			
+			print(img_location)
+			print(green_prec(os.getcwd() + img_location + "/" + filename))
+			file_data.write(filename + "," + img_location + "," + 	green_prec(os.getcwd() + img_location + "/" + filename) + "\n")	
 			print()
-	
-		
+			
+file_data.close()
 
- 
    
